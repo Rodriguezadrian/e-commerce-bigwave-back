9cd237c365529c49d8f20d5fd7e9312ab09356b3
Adrian Rodriguez <rgadrian.93@gmail.com>
2024-07-05 11:53:06 -0300

    Welcome message to api

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(routes);
+app.get("/", (req, res) => res.json({ msg: "Welcome to BigWave!" }));
 
 app.listen(process.env.APP_PORT, () => {
   console.log("Server is running on port 3000");
