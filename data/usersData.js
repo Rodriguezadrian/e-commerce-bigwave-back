bd29db711d02543600cd7fa51f17e1184ac4ead9
nachokapre <nachok1000@gmail.com>
2024-07-05 18:20:26 -0300

    token identificador de usuarios

--- data/usersData.js
+++ data/usersData.js
@@ -1,16 +1,14 @@
 module.exports = [
-  {
-    email: "carlitos@gmail.com",
-    password: "1234",
-    firstname: "Carlos",
-    lastname: "Gonzalez",
-    avatar: "https://cdn.iconscout.com/icon/free/png-256/avatar-380-456332.png",
-  },
-  {
-    email: "pepito@gmail.com",
-    password: "1234",
-    firstname: "Pepe",
-    lastname: "Rodriguez",
-    avatar: "https://cdn.iconscout.com/icon/free/png-256/avatar-380-456332.png",
-  },
+	{
+		email: 'carlitos@gmail.com',
+		password: '1234',
+		firstname: 'Carlos',
+		lastname: 'Gonzalez',
+	},
+	{
+		email: 'pepito@gmail.com',
+		password: '1234',
+		firstname: 'Pepe',
+		lastname: 'Rodriguez',
+	},
 ];
