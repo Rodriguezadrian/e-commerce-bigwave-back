77c6986cb62ec514c079ae7112daabb84d54ad94
Adrian Rodriguez <rgadrian.93@gmail.com>
2024-07-05 12:16:33 -0300

    update index.js

--- models/index.js
+++ models/index.js
@@ -4,18 +4,24 @@ const Product = require("./Product");
 const Order = require("./Order");
 const Category = require("./Category");
 
+const sequelizeOptions = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_CONNECTION,
+  logging: false,
+};
+
+if (process.env.DB_CONNECTION === "postgres") {
+  sequelizeOptions.dialectModule = require("pg");
+}
 
 const sequelize = new Sequelize(
   process.env.DB_DATABASE,
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_CONNECTION,
-    dialectModule: require("pg"),
-    logging: false,
-  }
+  sequelizeOptions
 );
+
 User.initModel(sequelize);
 Product.initModel(sequelize);
 Order.initModel(sequelize);
