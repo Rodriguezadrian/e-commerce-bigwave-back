1e4d629219fa059a6fd1158a8c98967c06e4f1a3
Juan Goyret <j.goyret1475@gmail.com>
2024-07-08 18:02:13 -0300

    protect admin routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/adminController");
+const { expressjwt: checkJwt } = require("express-jwt");
+const checkRole = require("../middleWares/permissionRequired");
 
+router.use(checkJwt({ secret: process.env.SECRET_JWT, algorithms: ["HS256"] })); //checkea el token
+router.use(checkRole.admin); //checkea el rol de admin
 router.get("/", adminController.index);
 router.post("/", adminController.store);
 router.get("/:id", adminController.show);
