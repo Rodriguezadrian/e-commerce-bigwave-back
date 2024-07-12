e1758e170296e5f7ee9e8d516a47090337b510e7
Adrian Rodriguez <rgadrian.93@gmail.com>
2024-07-12 16:10:37 -0300

    update show category routes

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -5,10 +5,10 @@ const checkRole = require("../middleWares/permissionRequired");
 const { expressjwt: checkJwt } = require("express-jwt");
 
 router.get("/", categoryController.index);
-router.use(checkJwt({ secret: process.env.SECRET_JWT, algorithms: ["HS256"] })); //checkea el token
-router.post("/", checkRole.admin, categoryController.store);
 router.get("/:slug", categoryController.show);
-router.patch("/:id", checkRole.admin, categoryController.update);
+router.use(checkJwt({ secret: process.env.SECRET_JWT, algorithms: ["HS256"] })); //checkea el token
+router.post("/", categoryController.store);
+router.patch("/:id", categoryController.update);
 router.delete("/:id", checkRole.admin, categoryController.destroy);
 
 module.exports = router;
