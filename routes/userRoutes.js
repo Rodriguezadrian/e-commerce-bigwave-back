4a8084d75f2d63baa445112ddfccd0446d4f57eb
Juan Goyret <j.goyret1475@gmail.com>
2024-07-15 19:19:47 -0300

    fix import missSinstax of middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 const { expressjwt: checkJwt } = require("express-jwt");
-const checkRole = require("../middleWares/permissionRequired");
+const checkRole = require("../middlewares/permissionRequired");
 
 router.post("/", userController.store); //guardar un nuevo usuario (admin o cliente)
 router.use(checkJwt({ secret: process.env.SECRET_JWT, algorithms: ["HS256"] })); //checkea el token
@@ -11,7 +11,11 @@ router.patch("/my-profile", userController.updateMyProfile); // actualizar mi pe
 router.delete("/my-profile", userController.destroyMyProfile); // eliminar mi perfil (cliente)
 
 router.get("/", checkRole.admin, userController.index); // mostrar todos los usuarios (admin)
-router.get( "/client-profile/:id",checkRole.admin,userController.showClient); // mostrar perfil de un cliente (admin)
-router.delete("/client-profile/:id",checkRole.admin,userController.destroyClient); // eliminar perfil de un cliente (admin)
+router.get("/client-profile/:id", checkRole.admin, userController.showClient); // mostrar perfil de un cliente (admin)
+router.delete(
+  "/client-profile/:id",
+  checkRole.admin,
+  userController.destroyClient
+); // eliminar perfil de un cliente (admin)
 
 module.exports = router;
